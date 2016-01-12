8d92e447730279402153c80e7c50a094a96605df
alhamood <alberthamood@gmail.com>
2016-01-11 22:13:23 -0500

    switched to app.run(port=33507)

--- app.py
+++ app.py
@@ -138,5 +138,6 @@ def show_data():
 	hourly_changes = (actual_starts-predicted_starts)
 		
 if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port, debug=False)
+  #port = int(os.environ.get("PORT", 5000))
+  #app.run(host='0.0.0.0', port=port, debug=False)
+  app.run(port=33507)
