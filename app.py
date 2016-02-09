ff035efdc0fc3ee0c79e231a07909674f5afe836
alhamood <alberthamood@gmail.com>
2016-02-09 18:49:47 -0500

    Final live heroic version with CSS styled templates

--- app.py
+++ app.py
@@ -400,6 +400,4 @@ def model_test():
 
 
 if __name__ == '__main__':
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port, debug=False)
-    app.run(port=33507, debug=True)
+    app.run(port=33507, debug=False)
